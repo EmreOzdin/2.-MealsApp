6f98b1454531f64163fb0d22e71541fc1ee25b81
Emre Özdin <mrzdnboun@gmail.com>
2024-02-26 18:57:17 +0300

    Update react-native-reanimated and related dependencies

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import FavoriteScreen from "./screens/FavoriteScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "white",
+        sceneContainerStyle: { backgroundColor: "#3f2f25" },
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{ title: "All Categories" }}
+      />
+      <Drawer.Screen
+        name="Favorites"
+        component={FavoriteScreen}
+        options={{ title: "Meal Details" }}
+      />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -22,10 +49,10 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={DrawerNavigator}
             options={{
-              title: "All Categories",
+              headerShown: false,
             }}
           />
           <Stack.Screen name="MealsOverView" component={MealsOverviewScreen} />
---
+++ screens/FavoriteScreen.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const FavoriteScreen = () => {
+  return <Text>The Favorite Screen!</Text>;
+};
+
+export default FavoriteScreen;
